8ad805df24b4b5f285475883ffd97ce897d3b2cd
JamesMlA <james.maradiaga@databuddies.com>
2024-08-17 15:47:32 -0600

    improbe-angular-base

--- frontend/public/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('clothesStore', ['ngRoute','clothesStore']);
-
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'public/partials/store.ejs',
-    controller: 'StoreController'
-  });
-}]);
